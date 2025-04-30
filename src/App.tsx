563716fab16a36512c7fff2babe5756edbea4b30
GABETROLL <gabriel7icplhappy@gmail.com>
2025-04-30 13:16:52 -0400

    Use the correct axii for checking if the player wants to move out-of-bounds.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import './App.css';
 import { ReactElement, useEffect, useState } from 'react';
 
 
-function Cell({value, selected, showInvalid}:
-    {value: number, selected: boolean, showInvalid: boolean}
+function Cell({value, selected, select, showInvalid}:
+    {value: number, selected: boolean, select: any, showInvalid: boolean}
   ) {
   let className = "cell";
   if (selected) className += " selected";
   if (showInvalid) className += " invalid";
 
   return (
-    <td className={className}>
+    <td className={className} onClick={select}>
       <p>
         {Board.validNumber(value) && value > 0 ? value.toString() : ''}
       </p>
@@ -30,16 +30,16 @@ function App() {
   useEffect(
     () => {
       function keyDownListener(event: KeyboardEvent) {
-        if (event.key === "a" && selectedCell[0] > 0) {
+        if (event.key === "a" && selectedCell[1] > 0) {
           setSelectedCell([selectedCell[0], selectedCell[1] - 1]);
           return;
-        } else if (event.key === "d" && selectedCell[0] < 8) {
+        } else if (event.key === "d" && selectedCell[1] < 8) {
           setSelectedCell([selectedCell[0], selectedCell[1] + 1]);
           return;
-        } else if (event.key === "w" && selectedCell[1] > 0) {
+        } else if (event.key === "w" && selectedCell[0] > 0) {
           setSelectedCell([selectedCell[0] - 1, selectedCell[1]]);
           return;
-        } else if (event.key === "s" && selectedCell[1] < 8) {
+        } else if (event.key === "s" && selectedCell[0] < 8) {
           setSelectedCell([selectedCell[0] + 1, selectedCell[1]]);
           return;
         }
@@ -73,6 +73,7 @@ function App() {
           key={`${y}${x}`}
           value={digit}
           selected={selectedCell !== null && selectedCell[0] === y && selectedCell[1] === x}
+          select={() => setSelectedCell([y, x])}
           showInvalid={showingInvalidCells && !(board.valid_cell(y, x, false))}
         />
       );
