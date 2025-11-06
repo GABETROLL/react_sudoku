0e3266c6e731c438e2550be21f51650e15fe5ff8
GABETROLL <gabriel7icplhappy@gmail.com>
2025-11-06 14:02:32 -0400

    Make Login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import Login, { LoginInfo } from "./Login";
+import Menu from "./Menu";
+
+type AppState = {
+  loginInfo?: LoginInfo,
+  name?: string,
+};
+
+
 export default function App() {
-  return (<button>Play</button>);
+  const [appState, setAppState]: [AppState, (appState: AppState) => void] = useState({});
+
+  if (appState.loginInfo) {
+    return <Menu />
+  } else {
+    return (<Login failed={() => {}} succeeded={(loginInfo: LoginInfo) => {setAppState({loginInfo})}} />);
+  }
 }
---
+++ src/Login.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { login } from "./dbApi";
+
+
+type LoginFormState = {
+    name: string,
+    email: string,
+    password: string,
+};
+
+
+function isHTMLInputElement(element: HTMLElement | null): element is HTMLInputElement {
+    return element !== null && Object.keys(element).includes('value');
+}
+
+
+function onChange(setLoginState: (state: LoginFormState) => void): void {
+    const name: HTMLElement | null = document.getElementById('name');
+    const email: HTMLElement | null = document.getElementById('email');
+    const password: HTMLElement | null = document.getElementById('password');
+
+    if (!isHTMLInputElement(name) || !isHTMLInputElement(email) || !isHTMLInputElement(password)) {
+        console.log(`ERROR: elements with ids "name", "email" and "password" were not inputs or were null!`);
+        return;
+    }
+
+    setLoginState({name: name.value, email: email.value, password: password.value});
+}
+
+
+export type LoginInfo = {
+    playerId: string,
+    sessionId: string,
+};
+
+
+function isLoginInfo(resJson: any): resJson is LoginInfo {
+    const resJsonKeys: string[] = Object.keys(resJson);
+
+    return (
+        typeof resJson === 'object'
+        && resJsonKeys.includes('playerId') && typeof resJson.playerId === 'string'
+        && resJsonKeys.includes('sessionId') && typeof resJson.sessionId === 'string'
+    );
+}
+
+
+async function onSubmit(loginState: LoginFormState, failed: () => void, succeeded: (resJson: LoginInfo) => void): Promise<void> {
+    try {
+        const res: Response = await login(loginState.name, loginState.email, loginState.password);
+
+        if (res.ok) {
+            const resJson: any = await res.json();
+
+            if (isLoginInfo(resJson)) {
+                return succeeded(resJson);
+            }
+        }
+    } catch (error) {
+
+    }
+
+    failed();
+}
+
+
+export default function Login({failed, succeeded}: {failed: () => void, succeeded: (resJson: LoginInfo) => void}) {
+    const [loginState, setLoginState]: [LoginFormState, (state: LoginFormState) => void] = useState({name: '', email: '', password: ''});
+
+    return (
+        <form>
+            <input id="name" type="name" value={loginState.name} onChange={() => onChange(setLoginState)} />
+            <input id="email" type="email" value={loginState.email} onChange={() => onChange(setLoginState)} />
+            <input id="password" type="password" value={loginState.password} onChange={() => onChange(setLoginState)} />
+            <button onClick={() => { onSubmit(loginState, failed, succeeded); }}>Submit</button>
+        </form>
+    );
+}
